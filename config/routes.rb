f551afb32740914e09d0eda857519f9c85cc944d
sunenilausen <sunenilausen@gmail.com>
2019-07-22 23:03:02 +0200

    rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :test_case_solutions, only: [:show, :update]
   resources :problems do
     resources :test_cases
