441161b84688e1edf5fd8f2b5cdb385d3143da1c
sunenilausen <sunenilausen@gmail.com>
2019-08-13 21:27:53 +0200

    can destroy solution with testcasesolutions

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,7 +1,7 @@
 class Solution < ApplicationRecord
   belongs_to :problem
   belongs_to :user
-  has_many :test_case_solutions
+  has_many :test_case_solutions, dependent: :destroy
 
   after_create :create_test_case_solutions
 
